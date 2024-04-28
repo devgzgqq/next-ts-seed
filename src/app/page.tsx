3ff5b32d992daeadac5ebc17e120b8c31e268d9c
顾争光 <gzgqq@126.com>
2024-04-28 22:03:07 +0800

    2024年4月28日 21:59:56

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-async function getData() {
-  const data = await fetch('https://localhost:3001/api');
-  return data.json();
-}
+// async function getData() {
+//   const data = await fetch('https://localhost:3001/api');
+//   return data.json();
+// }
 
 export default async function Home() {
-  const data = await getData();
+  const data = {
+    name: 'hello',
+  };
   return <article>{data.name}</article>;
 }
